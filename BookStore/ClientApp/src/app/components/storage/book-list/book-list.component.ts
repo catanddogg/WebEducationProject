9d170fa7385b796821bce89736434b6293f2c57e
simon <simon@code-care.pro>
2020-11-17 11:51:22 +0200

    Update typescript

--- BookStore/ClientApp/src/app/components/storage/book-list/book-list.component.ts
+++ BookStore/ClientApp/src/app/components/storage/book-list/book-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GetBookById } from 'src/app/shared/model/get-book.view';
 import { Router } from '@angular/router';
 import { HttpService } from 'src/app/shared/services/http.service';
+import { createChart } from 'lightweight-charts';
 
 @Component({
   selector: 'app-book-list',
@@ -16,6 +17,20 @@ export class BookListComponent implements OnInit {
      private httpService: HttpService) {
     this.userName = '';
     this.arrayBook = [];
+    const chart = createChart(document.body, { width: 400, height: 300 });
+    const lineSeries = chart.addLineSeries();
+    lineSeries.setData([
+        { time: '2019-04-11', value: 80.01 },
+        { time: '2019-04-12', value: 96.63 },
+        { time: '2019-04-13', value: 76.64 },
+        { time: '2019-04-14', value: 81.89 },
+        { time: '2019-04-15', value: 74.43 },
+        { time: '2019-04-16', value: 80.01 },
+        { time: '2019-04-17', value: 96.63 },
+        { time: '2019-04-18', value: 76.64 },
+        { time: '2019-04-19', value: 81.89 },
+        { time: '2019-04-20', value: 74.43 },
+    ]);
   }
 
   ngOnInit() {
